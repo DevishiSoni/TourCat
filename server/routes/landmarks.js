379b59f85e632a6313d4cba98bf6b91acd72b214
EveMurphy-Beaudet <eve.murphybeaudet@ontariotechu.net>
2026-04-14 17:46:42 -0400

    Finalized bcckend functionality

--- server/routes/landmarks.js
+++ server/routes/landmarks.js
@@ -41,14 +41,90 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-// Export the router
-export default router
+// Create a new landmark
+// Ask serviceLandmarks.js and send it back
+router.post('/', async (req, res) => {
+    try {
+        const newLandmark = req.body // get info from user
+        const created = await landmarkServices.createLandmark(newLandmark) // send to landmarksServices.js
 
-
-// POST (create)
-
+        res.status(201).json(created) // succesful code
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Failed to create landmark' }) // error
+    }
+})
 
 // PUT (update)
+// Update an existing landmark, find by ID
+// Ask serviceLandmarks.js and send it back
+router.put('/:id', async (req, res) => {
+    try {
+        const id = req.params.id // get ID to find specific landmark
+        const updatedData = req.body // get info from user
+        const updated = await landmarkServices.updateLandmark(id, updatedData) // send to landmarksServices.js
+
+        if(!updated){
+            res.status(404).json({ error: 'Landmark not found '}) //  can't find landmark
+        }
+
+        // Return updated object
+        res.json(updated)
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Failed to update landmark' }) // error
+    }
+})
 
 
 // DELETE (delete)
+// Delete an existing landmark, find by ID
+// Ask serviceLandmarks.js and send it back
+router.delete('/:id', async (req, res) => {
+    try{
+        const id = req.params.id
+        const deleted = await landmarkServices.deleteLandmark(id)
+        
+        if(!deleted){
+            res.status(404).json({ error: 'Landmark not deleted '}) //  can't find landmark
+        }
+
+        res.json({ message: 'Landmark deleted '})
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Failed to delete landmark' }) // error
+    }
+})
+
+// Export the router
+export default router
+
+// Filtering
+router.get('/', async (req, res) => {
+    try {
+        const filters = req.query
+
+        const data = await landmarkServices.getAllLandmarks(filters)
+
+        res.json(data)
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Failed to get landmarks' })
+    }
+})
+
+router.patch('/:id/favourite', async (req, res) => {
+    try {
+        const id = req.params.id
+        const updatedFavourite = await landmarkServices.updateFavourite(id)
+
+        if(!updatedFavourite){
+            return res.status(404).json({ error: 'Landmark not found with ID'})
+        }
+
+        res.json(updatedFavourite)
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Failed to update landmark favourite' })
+    }
+})
--- server/services/landmarkServices.js
+++ server/services/landmarkServices.js
@@ -15,11 +15,29 @@ const __dirname = path.dirname(__filename)
 
 const dataPath = path.join(__dirname, '../data/landmarks.json')
 
-// Get all landmarks
-export const getAllLandmarks = async () => {
+// Get all landmarks and get all landmarks filtered
+export const getAllLandmarks = async (filters = {}) => {
     const data = await fs.promises.readFile(dataPath, 'utf8')
     const json = JSON.parse(data)
-    return json.landmarks
+
+    let landmarks = json.landmarks
+
+    // Filter by city
+    if(filters.city){
+        landmarks = landmarks.filter(l => l.city.toLowerCase() === filters.city.toLowerCase())
+    }
+
+    // Filter by type
+    if(filters.type){
+        landmarks = landmarks.filter(l => l.type.toLowerCase() === filters.type.toLowerCase())
+    }
+
+    // Filter by country
+    if(filters.country){
+        landmarks = landmarks.filter(l => l.country.toLowerCase() === filters.country.toLowerCase())
+    }
+
+    return landmarks
 }
 
 // Get one landmark by id
@@ -29,8 +47,79 @@ export const getLandmarkById = async (id) => {
     return json.landmarks.find(l => l.id === parseInt(id))
 }
 
-// createLandmark()
+// Create a new landmark
+export const createLandmark = async (newLandmark) => {
+    const data = await fs.promises.readFile(dataPath, 'utf8')
+    const json = JSON.parse(data) // convert data from JSON to object
 
-// updateLandmark()
+    // Create new ID (last ID + 1 or 1 if landmarks is empty)
+    const newID = json.landmarks.length ? json.landmarks[json.landmarks.length-1].id + 1 : 1
+    // Build landmark object
+    const landmarkToAdd = {
+        id: newID,
+        ...newLandmark
+    }
+    // Adds to list
+    json.landmarks.push(landmarkToAdd)
+    // Pushes to file
+    await fs.promises.writeFile(dataPath, JSON.stringify(json, null, 2))
+    return landmarkToAdd
+}
 
-// deleteLandmark()
+// Update an existing landmark
+export const updateLandmark = async (updatedData) => {
+    const data = await fs.promises.readFile(dataPath, 'utf8')
+    const json = JSON.parse(data) // convert data from JSON to object
+
+    const index = json.landmarks.findIndex(l => l.id === parseInt(id))
+
+    if (index === -1){
+        return null //can't find
+    }
+
+    // update
+    json.landmarks[index] = {...json.landmarks[index], ...updatedData}
+
+    await fs.promises.writeFile(dataPath, JSON.stringify(json, null, 2))
+
+    return json.landmarks[index]
+}
+
+// Delete an existing landmark
+export const deleteLandmark = async (id) => {
+    const data = await fs.promises.readFile(dataPath, 'utf8')
+    const json = JSON.parse(data) // convert data from JSON to object
+
+    const index = json.landmarks.findIndex(l => l.id === parseInt(id))
+
+    if (index === -1){
+        return null //can't find
+    }
+
+    // remove landmark
+    const deletedLandmark = json.landmarks.splice(index, 1)
+
+    await fs.promises.writeFile(dataPath, JSON.stringify(json, null, 2))
+
+    return deletedLandmark[0]
+}
+
+// Favourites system (PATCH)
+export const updateFavourite = async (updatedFavourite) => {
+    const data = await fs.promises.readFile(dataPath, 'utf8')
+    const json = JSON.parse(data) // convert data from JSON to object
+
+    const landmark = json.landmarks.find(l => l.id === parseInt(id))
+
+    if (!landmark){
+        return null //can't find
+    }
+
+    // Flip
+    landmark.favourite = !landmark.favourite
+
+    // Write update to file
+    await fs.promises.writeFile(dataPath, JSON.stringify(json, null, 2))
+
+    return landmark
+}
